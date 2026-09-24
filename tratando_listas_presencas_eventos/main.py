271290bf243c06ad469652c06133c6badb4a46be
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2026-09-24 11:23:43 -0300

    resultado presenças sirc

--- tratando_listas_presencas_eventos/main.py
+++ tratando_listas_presencas_eventos/main.py
@@ -34,8 +34,21 @@ Util.carregar_arquivo_em_lista(
 )
 
 # 4. Ordenação e exibição (Usando o __lt__ que criamos antes)
+lista_inscritos.sort()
+Util.mostrar_lista(lista_inscritos)
+
 lista_terca_manha.sort()
 Util.mostrar_lista(lista_terca_manha)
+
+lista_terca_noite.sort()
+Util.mostrar_lista(lista_terca_noite)
+
+lista_quarta_manha.sort()
+Util.mostrar_lista(lista_quarta_manha)
+
+lista_quarta_noite.sort()
+Util.mostrar_lista(lista_quarta_noite)
+
 
 # 5. Otimização de Performance: Criar conjuntos (sets) para busca rápida O(1)
 # Buscar usando 'in' em listas dentro de um loop pode ser muito lento se houver muitos alunos.
@@ -47,6 +60,9 @@ sets_presenca = [
     set(lista_quarta_noite),
 ]
 
+
+print("====================== RESULTADO ========================")
+
 lista_nao_atestado = []
 
 # 6. Filtragem de alunos com menos de 3 presenças
@@ -54,5 +70,13 @@ for inscrito in lista_inscritos:
     # Sum soma 1 para cada True retornado na validação rápida do set
     total_presencas = sum(inscrito in lista_set for lista_set in sets_presenca)
 
-    if total_presencas < 3:
+    if total_presencas == 0:
         lista_nao_atestado.append(inscrito)
+
+
+# 7. Mostra os que não terao atestado
+
+
+print("Sem atestado: ", len(lista_nao_atestado))
+for aluno in lista_nao_atestado:
+    print(aluno)
